59dd72c2ea4159707f414634b956159bca901996
multiabhay <multiabhay@gmail.com>
2017-07-19 12:32:25 +0530

    "Rotation Euler added "

--- animation_nodes/nodes/Ninja/TransformOffset.py
+++ animation_nodes/nodes/Ninja/TransformOffset.py
@@ -11,10 +11,12 @@ class NinjaTestNode(bpy.types.Node, AnimationNode):
         self.newInput("Object", "Target", "target")
         self.newInput("Vector", "Offset", "offset")
         self.newInput("Vector", "Scale_Offset", "scale_offset")
+        self.newInput("Euler", "Rot_Offset", "rot_offset")
 
 
-    def execute(self,source,target,offset, scale_offset):
+    def execute(self,source,target,offset, scale_offset, rot_offset):
         if source is None or target is None:
             return
         target.location = source.location + offset
         target.scale = source.scale + scale_offset
+        target.rotation_euler = source.rotation_euler + rot_offset
