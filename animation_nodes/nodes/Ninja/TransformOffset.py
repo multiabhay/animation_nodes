54428594fd0754e93d103ed0e53019ee049d0894
multiabhay <multiabhay@gmail.com>
2017-07-19 19:51:30 +0530

    "something"

--- animation_nodes/nodes/Ninja/TransformOffset.py
+++ animation_nodes/nodes/Ninja/TransformOffset.py
@@ -1,22 +1,33 @@
 import bpy
+from bpy.props import *
+from mathutils import Vector
+from ... events import propertyChanged
 from ... base_types import AnimationNode
 
 class NinjaTestNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_NinjaTestNode"
     bl_label = "Ninja Transform Test"
 
+    useX = BoolProperty(name = "Use X", default = False, update = propertyChanged)
+    useY = BoolProperty(name = "Use Y", default = False, update = propertyChanged)
+    useZ = BoolProperty(name = "Use Z", default = False, update = propertyChanged)
+
     def create(self):
         #inputs
         self.newInput("Object", "Source", "source")
         self.newInput("Object", "Target", "target")
         self.newInput("Vector", "Offset", "offset")
-        self.newInput("Vector", "Scale_Offset", "scale_offset")
-        self.newInput("Euler", "Rot_Offset", "rot_offset")
+        #self.newOutput("Vector", "Target Location", "targetLocation")
+
+    def draw(self, layout):
+        layout.prop(self, "useX")
+        layout.prop(self, "useY")
+        layout.prop(self, "useZ")
 
 
-    def execute(self,source,target,offset, scale_offset, rot_offset):
+    def execute(self,source,target,offset):
         if source is None or target is None:
             return
-        target.location = source.location + offset
-        target.scale = source.scale + scale_offset
-        target.rotation_euler = source.rotation_euler + rot_offset
+        if self.useX : target.location.x = source.location.x + offset.x
+        if self.useY : target.location.y = source.location.y + offset.y
+        if self.useZ : target.location.z = source.location.z + offset.z
